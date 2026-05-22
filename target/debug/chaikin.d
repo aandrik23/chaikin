ac9efd0a684c8a652f3ce9237add73195d184309
sbusho <sofiaabusho@gmail.com>
2026-05-22 15:45:28 +0300

    start

---
+++ target/debug/chaikin.d
@@ -0,0 +1,1 @@
+/home/someuan/chaikin/target/debug/chaikin: /home/someuan/chaikin/src/main.rs
---
+++ target/debug/deps/chaikin-a83a3938c9db5255.d
@@ -0,0 +1,5 @@
+/home/someuan/chaikin/target/debug/deps/chaikin-a83a3938c9db5255.d: src/main.rs
+
+/home/someuan/chaikin/target/debug/deps/chaikin-a83a3938c9db5255: src/main.rs
+
+src/main.rs:
