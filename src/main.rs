822ea216293dc27c13f4a8e4984ad93970a21c25
sbusho <sofiaabusho@gmail.com>
2026-05-24 13:30:42 +0300

    Macroquad app shell added (B01)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+const WINDOW_WIDTH: f32 = 800.0;
+const WINDOW_HEIGHT: f32 = 600.0;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Chaikin".to_owned(),
+        window_width: WINDOW_WIDTH as i32,
+        window_height: WINDOW_HEIGHT as i32,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        if is_key_pressed(KeyCode::Escape) {
+            break;
+        }
+
+        clear_background(WHITE);
+
+        next_frame().await;
+    }
 }
